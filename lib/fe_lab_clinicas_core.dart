7d2ce3d383915ea5e088b63bde36192450cc9aa3
StephaneAntonieto <stephaneantonieto@gmail.com>
2025-01-22 15:21:17 -0300

    getit e asyncstate config

--- lib/fe_lab_clinicas_core.dart
+++ lib/fe_lab_clinicas_core.dart
@@ -5,3 +5,4 @@ export 'src/helpers/messages.dart';
 export 'src/loader/lab_clinicas_loader.dart';
 export 'src/restClient/rest_client.dart';
 export 'src/constants/local_storage_constants.dart';
+export 'src/lab_clinicas_core_config.dart';
